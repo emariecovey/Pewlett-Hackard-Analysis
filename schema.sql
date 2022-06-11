28145dfee5e4d512aedf68a734c8a5d5a1219a7c
Emarie Swenson <emarieswenson@Emaries-MacBook-Air.local>
2022-06-11 12:54:06 -0600

    successfully created and imported tables

--- schema.sql
+++ schema.sql
@@ -16,15 +16,16 @@ CREATE TABLE employees(
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager(
-	dept_no varchar(4) NOT NULL,
-	emp_no int NOT NULL, 
-	from_date date NOT NULL,
-	to_date date NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no, emp_no)
-	);
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+	
 
 CREATE TABLE salaries (
 	emp_no int NOT NULL,
@@ -42,7 +43,7 @@ CREATE TABLE dept_emp(
 	to_date date NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,dept_no)
 );
 
 CREATE TABLE titles(
@@ -51,7 +52,17 @@ CREATE TABLE titles(
 	from_date date NOT NULL,
 	to_date date NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,title,from_date)
 );
 
+--dropped tables when trying to figure out error
+--DROP TABLE dept_manager CASCADE;
+
+--all tables imported successfully after adjusting primary keys
 SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM titles
+
